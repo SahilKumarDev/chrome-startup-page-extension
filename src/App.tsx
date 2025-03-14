9b11db51d0df6bab1af5d4e5aaed95c28f0a3c08
sahilkumardev <sahilkumar710@outlook.com>
2025-03-14 19:26:52 +0530

    update the changes

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="flex items-center justify-center text-center  h-screen text-3xl w-screen">
+      Build on chrome extension of death clock
+    </div>
+  );
+};
+
+export default App;
